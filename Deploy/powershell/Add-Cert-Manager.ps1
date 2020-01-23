b3ac36aa90255f6854dfe09236d2a0f08e146c10
Colin Dembovsky <colindembovsky@gmail.com>
2020-01-23 14:38:22 -0800

    Adding updated cert-manager logic

--- Deploy/powershell/Add-Cert-Manager.ps1
+++ Deploy/powershell/Add-Cert-Manager.ps1
@@ -1,4 +1,31 @@
 #! /usr/bin/pwsh
 
-# Installs cert-manager on cluster
-Invoke-Expression "helm install --name cert-manager --namespace kube-system  --version v0.4.1 stable/cert-manager"
+Param (
+    [parameter(Mandatory=$true)][string]$resourceGroup,
+    [parameter(Mandatory=$false)][string]$aksName
+)
+
+if ([string]::IsNullOrEmpty($aksName)) {
+    Write-Host "No AKS name. Quering resourceGroup $resourceGroup to calculate name" -ForegroundColor Yellow
+    $aksName = $(az aks list -g $resourceGroup -o json | ConvertFrom-Json).name
+    Write-Host "AKS Name = $aksName"
+}
+
+Write-Host "Getting k8s cluster credentials"
+az aks get-credentials -g $resourceGroup -n $aksName
+
+Write-Host "--------------------------------------------------------" -ForegroundColor Yellow
+Write-Host " Enabling Cert Manager on cluster $aksName in RG $resourceGroup"  -ForegroundColor Yellow
+Write-Host " --------------------------------------------------------" -ForegroundColor Yellow
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
+
+# Install the cert-manager Helm chart
+helm install \
+  --name cert-manager \
+  --namespace cert-manager \
+  --version v0.4.1 \
+  jetstack/cert-manager
