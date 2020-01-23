b9483334af66f4e564a53ae687b5bc2f5102f68b
Colin Dembovsky <colindembovsky@gmail.com>
2020-01-23 14:57:17 -0800

    Trying to fix cert-manager bugs

--- Deploy/powershell/Add-Cert-Manager.ps1
+++ Deploy/powershell/Add-Cert-Manager.ps1
@@ -14,6 +14,12 @@ if ([string]::IsNullOrEmpty($aksName)) {
 Write-Host "Getting k8s cluster credentials"
 az aks get-credentials -g $resourceGroup -n $aksName
 
+$ns = $(kubectl get namespaces).Contains("cert-manager") | Out-string
+if (!$ns.Contains("cert-manager")) { kubectl create namespace cert-manager }
+
+# workaround for bug for cert-manager with Helm3
+kubectl config set-context $aksName --namespace cert-manager
+
 Write-Host "--------------------------------------------------------" -ForegroundColor Yellow
 Write-Host " Enabling Cert Manager on cluster $aksName in RG $resourceGroup"  -ForegroundColor Yellow
 Write-Host " --------------------------------------------------------" -ForegroundColor Yellow
@@ -24,8 +30,8 @@ helm repo update
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
 
 # Install the cert-manager Helm chart
-helm install \
-  --name cert-manager \
+helm upgrade \
+  cert-manager \
   --namespace cert-manager \
   --version v0.4.1 \
   jetstack/cert-manager
